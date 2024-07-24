d071acb8f6906a1df644e1b5792b3c6e16a10c68
LinoMayer <linomayer71@gmail.com>
2024-07-25 01:22:20 +0200

    first commit

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -2,29 +2,29 @@
 // The config you add here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+//import * as Sentry from "@sentry/nextjs";
 
-Sentry.init({
-  dsn: "https://3886c77c9bc2a8fecf53e23f0506ef52@o4507642814791680.ingest.de.sentry.io/4507642822393936",
+//Sentry.init({
+//  dsn: "https://3886c77c9bc2a8fecf53e23f0506ef52@o4507642814791680.ingest.de.sentry.io/4507642822393936",
 
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+//  tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+//  debug: false,
 
-  replaysOnErrorSampleRate: 1.0,
+//  replaysOnErrorSampleRate: 1.0,
 
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+//  replaysSessionSampleRate: 0.1,
 
   // You can remove this option if you're not planning to use the Sentry Session Replay feature:
-  integrations: [
-    Sentry.replayIntegration({
-      // Additional Replay configuration goes in here, for example:
-      maskAllText: true,
-      blockAllMedia: true,
-    }),
-  ],
-});
+//  integrations: [
+//    Sentry.replayIntegration({
+//      // Additional Replay configuration goes in here, for example:
+//      maskAllText: true,
+//      blockAllMedia: true,
+//    }),
+//  ],
+//});
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -2,18 +2,18 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+//import * as Sentry from "@sentry/nextjs";
 
-Sentry.init({
-  dsn: "https://3886c77c9bc2a8fecf53e23f0506ef52@o4507642814791680.ingest.de.sentry.io/4507642822393936",
+//Sentry.init({
+  //dsn: "https://3886c77c9bc2a8fecf53e23f0506ef52@o4507642814791680.ingest.de.sentry.io/4507642822393936",
 
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+  //tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+  //debug: false,
 
   // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
   // spotlight: process.env.NODE_ENV === 'development',
   
-});
+//});
